abd4e549f8adbf2418f1d04dd9fc702e840b81fa
[kaitlyn.helstrom] <[krhelstrom@hotmail.com]>
2018-05-17 11:28:50 -0600

    added styling and stuff

--- 2018_krh_portfolio_submit/src/App.js
+++ 2018_krh_portfolio_submit/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './project-images/kaitlyn.png';
 import './App.css';
+import ContactPanel from './contactpanel.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+     
+        <div className="App-intro">
+        <img className="profPic" src={require("./project-images/kaitlyn.png")} alt="profile" />
+
+        <h1 className="App-title">KAITLYN HELSTROM</h1>
+
+        </div>
+
+        <ContactPanel />
+
+
       </div>
     );
   }
---
+++ 2018_krh_portfolio_submit/src/contactpanel.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './contactpanel.css';
+
+
+
+
+export default class ContactPanel extends React.Component {
+    render() {
+
+        return (
+            <div class="maincontainer">
+             <div className='notitle'>
+            <div/>
+                
+
+
+                <div className="container">
+
+
+                    <div className='contacttext'>
+                        <h1> <small>Contact</small></h1>
+                        Kaitlyn Helstrom <br />
+                        406.570.3222<br />
+                        krhelstrom@hotmail.com<br />
+                       <div className='skills'> <h1> <small>Skills</small></h1>
+                        -  Proficient in Adobe InDesign, Photoshop, Bridge, Illustrator <br />
+                        -  Understands how concise and informative visual systems should work.<br />
+                        If you have to explain it, it's not working. <br />
+                        <br />
+                        </div>
+                    </div>
+                    <div className='profiletext'>
+                        <h1>Professional Profile</h1>
+                       <p> I mean really like it? I mean if money were no object, would you still be doing what you get out of bed for in the morning every day?  If no, then why? In today's age there is no excuse to not be doing something that you love. For some it might be more difficult because of personal circumstances but there is always a way to do something that you care about and are passionate about.  This might include starting a side business to work at alongside your daily job. This might mean quitting your current job and jumping all in. It might mean breaking free of people expectations and simply following your heart.  To be able to pour the heart into a brand with meaning & intention. 
+                            If you don't then what's the point of life? What's the point of living, if just for someone else? Think about that before you wake up tomorrow.
+                            If someone wants to start there own business in this town, you gotta have the ability to transcend traditional ideas, rules, and make it funky. to create meaningful new concepts, actually solve a problem, and be able to actualize a functional concept
+                             my resume has been upload
+                             </p>
+                    </div>
+                
+                   </div>
+                   </div>
+                    </div>
+                            );
+                        }
+                    }
+                
