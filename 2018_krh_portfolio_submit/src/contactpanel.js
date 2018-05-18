f478c647eca40bc688c95dc4e81d02f7d36b1ea2
[kaitlyn.helstrom] <[krhelstrom@hotmail.com]>
2018-05-17 21:58:04 -0600

    fixed more styling

--- 2018_krh_portfolio_submit/src/contactpanel.js
+++ 2018_krh_portfolio_submit/src/contactpanel.js
@@ -26,15 +26,13 @@ export default class ContactPanel extends React.Component {
                         -  Proficient in Adobe InDesign, Photoshop, Bridge, Illustrator <br />
                         -  Understands how concise and informative visual systems should work.<br />
                         If you have to explain it, it's not working. <br />
+                        -  
                         <br />
                         </div>
                     </div>
                     <div className='profiletext'>
                         <h1>Professional Profile</h1>
-                       <p> I mean really like it? I mean if money were no object, would you still be doing what you get out of bed for in the morning every day?  If no, then why? In today's age there is no excuse to not be doing something that you love. For some it might be more difficult because of personal circumstances but there is always a way to do something that you care about and are passionate about.  This might include starting a side business to work at alongside your daily job. This might mean quitting your current job and jumping all in. It might mean breaking free of people expectations and simply following your heart.  To be able to pour the heart into a brand with meaning & intention. 
-                            If you don't then what's the point of life? What's the point of living, if just for someone else? Think about that before you wake up tomorrow.
-                            If someone wants to start there own business in this town, you gotta have the ability to transcend traditional ideas, rules, and make it funky. to create meaningful new concepts, actually solve a problem, and be able to actualize a functional concept
-                             my resume has been upload
+                       <p> I would like to work in an environment where i will be able to pour my heart into a company or brand by doing meaningful and intentional work.
                              </p>
                     </div>
                 
